757a19a2561a70e1b4cac09bd9e6d009b5b25ac3
takuowake <takuowake@gmail.com>
2023-07-11 16:45:42 +0900

    delete unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:webview_flutter/webview_flutter.dart' as webview_flutter;
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,14 +18,14 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Container(
+            SizedBox(
               height: 300,
               child: webview_flutter.WebViewWidget(
                 controller: webview_flutter.WebViewController()
                     ..loadRequest(Uri.parse('https://flutter.dev')),
               ),
             ),
-            Container(
+            SizedBox(
               height: 300,
               child: InAppWebView(
                 initialUrlRequest: URLRequest(
